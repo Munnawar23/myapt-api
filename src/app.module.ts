47cf42d8a54eb52abe6831131ebbf8b8042756df
Shekhar <shekharkalyana5@gmail.com>
2025-07-12 13:55:21 +0530

    db and port

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,11 +47,11 @@ import { AnnouncementsModule } from './announcements/announcements.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: '35.202.54.187',
       port: 5432,
       username: 'postgres',
-      password: 'postgres',
-      database: 'my_apt_db',
+      password: '3102aaC:C805MCnL',
+      database: 'apartment_db',
       entities: [
         User,
         Role,
@@ -103,4 +103,4 @@ import { AnnouncementsModule } from './announcements/announcements.module';
     },
   ],
 })
-export class AppModule {}
+export class AppModule { }
